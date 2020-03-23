7d794c4a255ea416d16b1eaa2f4450387fe0e055
Josh Godinez <josh.jsx@gmail.com>
2020-03-23 15:10:08 -0700

    created skeleton for landing page

---
+++ budee-checkers/src/containers/Landing/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>test</div>;
+  }
+}
+
+export default Landing;
