89315db22a98d91ce385ce0c08822fa7492aa421
Josh Godinez <josh.jsx@gmail.com>
2020-03-23 15:10:42 -0700

    added landing component to App

--- budee-checkers/src/App.js
+++ budee-checkers/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Landing from "./containers/Landing";
 
 function App() {
   return (
     <div className="App">
+      <Landing/>
     </div>
   );
 }
